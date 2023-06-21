c624b203e79e9f148e06d5e9dbe40b79d8f689cf
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-21 23:23:18 +0000

    Update CurrencyPage.js

--- src/transaction/CurrencyPage.js
+++ src/transaction/CurrencyPage.js
@@ -5,16 +5,16 @@ import Col from 'react-bootstrap/Col';
 const CurrencyPage = () => {
   const [rate, getRate] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://data.fixer.io/api/latest?access_key=b71430f5e7f1f8ca97f3a5be11eab7b7'
-      )
-      .then((review) => {
-        getReview(review.);
-      })
-      .catch((errr) => {
-        console.log(errr);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('http://data.fixer.io/api/latest?access_key=b71430f5e7f1f8ca97f3a5be11eab7b7'
+  //     )
+  //     .then((review) => {
+  //       getReview(review.);
+  //     })
+  //     .catch((errr) => {
+  //       console.log(errr);
+  //     });
+  // }, []);
 
   return (
     <Col style={{ textAlign: 'center' }}>
