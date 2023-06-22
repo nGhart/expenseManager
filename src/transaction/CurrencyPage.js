18848bc3468317e1b14c5f39dbbff0f979164037
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-22 18:13:02 +0000

    Update CurrencyPage.js fetch

--- src/transaction/CurrencyPage.js
+++ src/transaction/CurrencyPage.js
@@ -5,21 +5,21 @@ import Col from 'react-bootstrap/Col';
 const CurrencyPage = () => {
   const [rate, getRate] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get('http://data.fixer.io/api/latest?access_key=b71430f5e7f1f8ca97f3a5be11eab7b7'
-  //     )
-  //     .then((review) => {
-  //       getReview(review.);
-  //     })
-  //     .catch((errr) => {
-  //       console.log(errr);
-  //     });
-  // }, []);
+  useEffect(() => {
+     axios.get('http://data.fixer.io/api/latest?access_key=b71430f5e7f1f8ca97f3a5be11eab7b7'
+       )
+       .then((review) => {
+         getReview(review.data.GHS);
+       })
+       .catch((errr) => {
+         console.log(errr);
+       });
+   }, []);
 
   return (
     <Col style={{ textAlign: 'center' }}>
       <h3>US Dollar Rate</h3>
-      <span>1 USD:</span>x GHC
+      <span>1 USD:</span>{rate} GHC
       <span></span>
     </Col>
   );
