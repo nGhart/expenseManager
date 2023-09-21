e1536689fd273b596aa4d3ddd8058323e843c365
nGhart <naaghart@gmail.com>
2023-09-21 16:22:06 +0000

    usd

--- src/transaction/CurrencyPage.js
+++ src/transaction/CurrencyPage.js
@@ -5,28 +5,28 @@ import Col from 'react-bootstrap/Col';
 const CurrencyPage = () => {
   const [dollarRate, setDollarRate] = useState([]);
 
-  // useEffect(() => {
-  //   const getDollarRate = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         'https://openexchangerates.org/api/latest.json?app_id=8c5690895ad74c588012c10fc4bb6da0'
-  //       );
-  //       const data = await response.json();
-  //       setDollarRate(data.rates.GHS);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+  useEffect(() => {
+    const getDollarRate = async () => {
+      try {
+        const response = await fetch(
+          'https://openexchangerates.org/api/latest.json?app_id=8c5690895ad74c588012c10fc4bb6da0'
+        );
+        const data = await response.json();
+        setDollarRate(data.rates.GHS);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  //   getDollarRate();
-  // });
+    getDollarRate();
+  });
 
   return (
     <Col style={{ textAlign: 'center' }}>
       <h3>Exchange rate</h3>
       <p>
-        {/* <span style={{ fontSize: '20px', fontWeight: 700 }}>1 </span>USD :
-        <span style={{ fontSize: '20px', fontWeight: 700 }}>{dollarRate} </span> */}
+        <span style={{ fontSize: '20px', fontWeight: 700 }}>1 </span>USD :
+        <span style={{ fontSize: '20px', fontWeight: 700 }}>{dollarRate} </span>
         GHC
       </p>
     </Col>
